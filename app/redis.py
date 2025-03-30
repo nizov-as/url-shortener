03965b9e4581809359911cc145715d7dd8eb7d64
Alexander Nizov <nizoff.as@yandex.ru>
2025-03-31 01:34:01 +0300

    refactor: remove unused REDIS_HOST and REDIS_PORT from redis.py

--- app/redis.py
+++ app/redis.py
@@ -1,7 +1,8 @@
-import os
+#import os
 import redis.asyncio as redis
+from app.config import settings
 
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+#REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+#REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+redis_client = redis.from_url(settings.REDIS_URL, decode_responses=True)
